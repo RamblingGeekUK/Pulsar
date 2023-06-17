a12893592a52d06f3adb1426903e0778de9fec0a
Wayne Taylor <wayne@kryptos.co.uk>
2023-06-17 22:19:19 +0100

    Started working on the picks page... picking randomly but not always 3 cars

---
+++ Pages/Packs/index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pulsar.Data;
+using Pulsar.Models;
+
+namespace Pulsar.Pages.Packs
+{
+    public class indexModel : PageModel
+    {
+        private readonly Pulsar.Data.PulsarContext _context;
+
+        public indexModel(Pulsar.Data.PulsarContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Vechicles> Vechicles { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Vechicles != null)
+            {
+
+                var rnd = new Random();
+                
+
+                Vechicles = await _context.Vechicles.ToListAsync();
+
+                var x = Vechicles.Count;
+
+                int packpick = rnd.Next(x);
+
+                Vechicles = await _context.Vechicles.Skip(packpick).Take(3).ToListAsync();
+
+                
+            }
+        }
+    }
+}
